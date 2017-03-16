344d1f751daa8d784b2ed22895430a410268b1d6
dragonpan2 <panbeining@hotmail.com>
2017-03-16 10:22:51 -0400

    BN: AI

--- Fregolia/Fregolia/AI.cpp
+++ Fregolia/Fregolia/AI.cpp
@@ -1,35 +1,217 @@
 #include "Ai.h"
 #include "externalIncludes.h"
+#include <stdlib.h>     /* srand, rand */
 
-Ai::Ai() {
-
-}
-
-Ai::~Ai() {
+Ai::Ai()
+{
 
 }
 
-void Ai::stateSetter(glm::vec2 thisPos, glm::vec2 playerPos,glm::vec2 thisDirection , bool isPlayerSneak, int previousState) {
-
-}
-void Ai::greenState(glm::vec2 thisPos,glm::vec2 thisDirection) {
-
-}
-
-void Ai::yellowState() {
+Ai::~Ai()
+{
 
 }
 
-void Ai::redState() {
+int Ai::stateSetter(glm::vec2 thisPos, glm::vec2 playerPos,glm::vec2 thisDirection, bool isPlayerSneak, int previousState, int lastAction)
+{
 
+    switch (previousState)
+    {
+    case 1: //if it was on green state
+        //detection code here
+        if (thisDirection ==glm::vec2  (-1.0f,0.0f) )
+        {
+            if (glm::length(thisPos - playerPos) <= greenDetectionRange)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else if (thisDirection == glm::vec2 (1.0f,0.0f))
+        {
+            if (glm::length(playerPos - thisPos) <= greenDetectionRange)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        break;
+    case 2:
+        if (thisDirection == glm::vec2 (-1.0f,0.0f) )
+        {
+            if (glm::length(thisPos - playerPos) <= yellowOrangeDetectionRange)
+            {
+                return 3;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else if (thisDirection == glm::vec2 (1.0f,0.0f))
+        {
+            if (glm::length(playerPos - thisPos) <= yellowOrangeDetectionRange)
+            {
+                return 3;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        break;
+    case 3:
+        if (thisDirection == glm::vec2 (-1.0f,0.0f) )
+        {
+            if (glm::length(thisPos - playerPos) <= redDetectionRange)
+            {
+                return 3;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+        else if (thisDirection == glm::vec2 (1.0f,0.0f))
+        {
+            if (glm::length(playerPos - thisPos) <= redDetectionRange)
+            {
+                return 3;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+        break;
+    case 4:
+        if (thisDirection == glm::vec2 (-1.0f,0.0f) )
+        {
+            if (glm::length(thisPos - playerPos) <= yellowOrangeDetectionRange)
+            {
+                return 3;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else if (thisDirection == glm::vec2 (1.0f,0.0f))
+        {
+            if (glm::length(playerPos - thisPos) <= yellowOrangeDetectionRange)
+            {
+                return 3;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        break;
+    }
+    return 1;
+}
+int Ai::greenState(glm::vec2 thisPos,glm::vec2 thisDirection)
+{
+    switch (rand() % 7+1)
+    {
+    case 1:
+        return 1;
+        break;
+    case 2:
+        if (thisDirection == glm::vec2 (-1.0f,0.0f))
+        {
+            return 2;
+        }
+        else return 1;
+    case 3:
+        if (thisDirection == glm::vec2 (1.0f,0.0f))
+        {
+            return 3;
+        }
+        else return 1;
+    case 4:
+        if (lastAction != 4) {
+            return 4;
+        }
+    default : return 1;
+    }
+    //behavior on green here
+    return 1;
 }
 
-void Ai::orangeState() {
+int Ai::yellowState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection)
+{
+    if ((thisPos.x - playerPos.x > 0) && (thisDirection == glm::vec2 (-1.0f,0.0f)) ) {
+        return 2;
+    }
+    if (playerPos.x - thisPos.x > 0 && thisDirection == glm::vec2 (1.0f,0.0f) ) {
+        return 3;
+    }
 
+
+return 1;
 }
 
-int Ai::AiMethode() {
+int Ai::redState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection)
+{
+    if (thisPos.x - playerPos.x > 0 && thisDirection == glm::vec2 (-1.0f,0.0f) ) { //si le joueur est a droite du AI et le AI fais face au joueur
+        if (glm::length(thisPos - playerPos) < attackRange) { //si le joueur est dans la distance dattack du ai
+            return 5;
+        }
+        else {
+            return 2;
+        }
+    }
+    else {
+        return 4;
+    }
 
-return 0;
+    if (playerPos.x - thisPos.x > 0 && thisDirection == glm::vec2 (1.0f,0.0f) ) { //si le joueur est a gauche du AI et le AI fais face au joueur
+        if (glm::length(playerPos -thisPos) < attackRange) { //si le joueur est dans la distance dattack du ai
+            return 5;
+        }
+        else {
+            return 3;
+        }
+    }
+    else {
+        return 4;
+    }
+
+    return 1;
+}
+
+int Ai::orangeState()
+{
+
+
+
+return 1;
+}
+
+int Ai::AiMethode(glm::vec2 thisPos,glm::vec2 playerPos, glm::vec2 thisDirection, bool isPlayerSneak, int previousState)
+{
+
+    switch (stateSetter(thisPos,playerPos,thisDirection,  isPlayerSneak, previousState,  lastAction))
+    {
+    case 1:
+        return greenState(thisPos,thisDirection);
+    case 2:
+        return yellowState(thisPos,  playerPos, thisDirection);
+    case 3:
+        return redState( thisPos,  playerPos,  thisDirection);
+    case 4:
+        return orangeState();
+
+    }
+
+    return 0;
 }
 
--- Fregolia/Fregolia/Ai.h
+++ Fregolia/Fregolia/Ai.h
@@ -8,14 +8,23 @@ class Ai
     public:
         Ai();
         virtual ~Ai();
+
+        //var
         int previousState; //1 green, 2 yellow, 3 red,4 orange
+        int lastAction; //1 wait, 2 go left, 3 go right, 4 change direction,5 attack, 6 jump left, 7 jump right,
         bool aiActive;
-        void stateSetter(glm::vec2 thisPos, glm::vec2 playerPos,glm::vec2 thisDirection , bool isPlayerSneak, int previousState);
-        void greenState(glm::vec2 thisPos,glm::vec2 thisDirection);
-        void yellowState();
-        void redState();
-        void orangeState();
-        int AiMethode();
+        int greenDetectionRange;
+        float yellowOrangeDetectionRange;
+        float redDetectionRange;
+        float attackRange;
+
+        //m
+        int stateSetter(glm::vec2 thisPos, glm::vec2 playerPos,glm::vec2 thisDirection , bool isPlayerSneak, int previousState, int lastAction);
+        int greenState(glm::vec2 thisPos,glm::vec2 thisDirection);
+        int yellowState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection);
+        int redState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection);
+        int orangeState();
+        int AiMethode(glm::vec2 thisPos,glm::vec2 playerPos, glm::vec2 thisDirection, bool isPlayerSneak, int previousState);
 
     protected:
 
