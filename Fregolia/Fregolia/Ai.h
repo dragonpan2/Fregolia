b2e6840ad463182293d25c883ec257c3f4d6dcff
dragonpan2 <panbeining@hotmail.com>
2017-04-02 17:47:12 -0400

    bug fix

--- Fregolia/Fregolia/Ai.h
+++ Fregolia/Fregolia/Ai.h
@@ -12,21 +12,21 @@ class Ai
         //var
         int previousState; //1 green, 2 yellow, 3 red,4 orange
         int lastAction; //1 wait, 2 go left, 3 go right, 4 change direction,5 attack, 6 jump left, 7 jump right,
-        bool aiActive;
-        int greenDetectionRange;
-        float yellowOrangeDetectionRange;
-        float redDetectionRange;
-        float attackRange;
+        bool aiActive = true;
+        int greenDetectionRange = 40;
+        //int greenDetectionRange;
+        float yellowOrangeDetectionRange = 30;
+        float redDetectionRange = 20;
+        float attackRange = 5;
+      //  glm::vec2 thisDirection = glm::vec2(1.0f,0.0f);
 
         //m
         int stateSetter(glm::vec2 thisPos, glm::vec2 playerPos,glm::vec2 thisDirection , bool isPlayerSneak, int previousState, int lastAction);
         int greenState(glm::vec2 thisPos,glm::vec2 thisDirection);
         int yellowState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection);
         int redState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection);
-        int orangeState();
+        int orangeState(glm::vec2 thisPos, glm::vec2 playerPos, glm::vec2 thisDirection);
         int AiMethode(glm::vec2 thisPos,glm::vec2 playerPos, glm::vec2 thisDirection, bool isPlayerSneak, int previousState);
-
-
     protected:
 
     private:
@@ -34,3 +34,4 @@ class Ai
 
 
 #endif // AI_H_INCLUDED
+
